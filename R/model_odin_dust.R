e2811513f06ef96b19958d5df1770f4f46fdf8e9
alxsrobert <alexis3.robert3@gmail.com>
2022-10-17 15:23:38 +0100

    Initial commit

---
+++ R/model_odin_dust.R
@@ -0,0 +1,220 @@
+#### First go at the age and region-structured model
+#### Include transmission between regions (using a spatial kernel) and age groups
+#### Include outside importations
+
+#### Todo:
+#### - Add movement between age groups
+#### - Add changes in the distribution of vaccine status through time
+#### - Draw the number of importations in this script (currently defined by the user)
+
+#### Structure of the script:
+#### 1- Core equations
+#### 2- Draw number of individuals changing compartments
+#### 3- Compute the probability of movement between compartments
+#### 4- compute the force of infection
+#### 5- Compute the number of importations
+#### 6- Define initial status / dimension of the compartment matrices
+
+### 1- Core equations for transitions between compartments:
+### All compartments are stratified by age (ROWS) and regions (COLUMNS)
+## Susceptibles compartments: 
+# New value of S, V1, and V2 is: 
+#   (old value) - (nb of susceptible individuals who were exposed)
+update(S[,]) <- S[i, j] - n_SEs[i, j]
+update(V1[,]) <- V1[i, j] - n_v1E[i, j]
+update(V2[,]) <- V2[i, j] - n_v2E[i, j]
+
+## Exposed compartments: 
+# New value of ES, EV1, and EV2 is: 
+#   (old value) + (new_exposed) - (nb of exposed individuals moving to infectious)
+update(Es[,]) <-  Es[i, j] + n_SEs[i, j] - n_EsIs[i, j]
+update(Ev1[,]) <- Ev1[i, j] + n_v1E[i, j] - n_Ev1Iv1[i, j]
+update(Ev2[,]) <- Ev2[i, j] + n_v2E[i, j] - n_Ev12v2[i, j]
+
+## Infected compartments: 
+# New value of IS is:
+#   (old value) + (new_exposed) + (new_imports) - (nb of infectious moving to recovered)
+# import_t corresponds to the number of new imports in this region / age group
+# Assumption: All imports are unvaccinated
+update(Is[,]) <- Is[i, j] + n_EsIs[i, j] - n_IsR[i, j] + import_t[i,j]
+update(Iv1[,]) <- Iv1[i, j] + n_Ev1Iv1[i, j] - n_Iv1R[i, j]
+update(Iv2[,]) <- Iv2[i, j] + n_Ev12v2[i, j] - n_Iv2R[i, j]
+
+## Recovered compartment: 
+update(R[,]) <- R[i, j] + n_IsR[i, j] + n_Iv1R[i, j] + n_Iv2R[i, j]
+
+
+### 2- Compute "n_" variables: the number of individuals changing between compartments
+## Draw from binomial distributions: 
+# Depends on the number of individuals in the compartment and the probability of moving
+n_SEs[,] <- rbinom(S[i, j], p_SE[i, j])
+n_v1E[,] <- rbinom(V1[i, j], p_SEv1[i, j])
+n_v2E[,] <- rbinom(V2[i, j], p_SEv2[i, j])
+
+n_EsIs[,] <- rbinom(Es[i, j], p_EI)
+n_Ev1Iv1[,] <- rbinom(Ev1[i, j], p_EI)
+n_Ev12v2[,] <- rbinom(Ev2[i, j], p_EI)
+
+n_IsR[,] <- rbinom(Is[i, j], p_IR)
+n_Iv1R[,] <- rbinom(Iv1[i, j], p_IR)
+n_Iv2R[,] <- rbinom(Iv2[i, j], p_IR)
+
+
+
+### 3- Compute the probabilities of transition between each compartment:
+## From S to E depends on lambda_t, the force of infection (per age / region / date)
+p_SE[,] <- 1 - exp(-lambda_t[i, j] * dt) # S to E
+## From v1 and v2 to E also depends on lambda, and v1/v2, the protection from infection 
+## brought by the vaccine 
+p_SEv1[,] <- 1 - exp(-lambda_t[i, j] * v1 * dt) # V1 to Ev1
+p_SEv2[,] <- 1 - exp(-lambda_t[i, j] * v2 * dt) # V2 to Ev2
+## From E to I depends on the duration of the latent period
+p_EI <- 1 - exp(-alpha * dt) # E to I
+## From I to R depends on the duration of the infectious period
+p_IR <- 1 - exp(-gamma * dt) # I to R
+
+## Define the duration of  the latent / infectious periods
+alpha <- user(0.1)
+gamma <- user(0.1)
+
+## Define the default protection from infection brought by the vaccine
+v1 <- user(.5)
+v2 <- user(.1)
+
+
+
+### 4- Compute lambda_t, the force of infection including the impact of seasonnality
+lambda_t[,] <- lambda[i,j] * 
+  exp(X * cos(2 * 3.14 * time / 365) + Y * sin(2 * 3.14 * time / 365))
+# Default values of X and Y (seasonnality parameters)
+X <- user(1)
+Y <- user(1)
+
+### Compute the force of infection from beta (the average nb of contacts), 
+### N (the total number of inhabitants) and case_ij (the potential of infection
+### stemming from the currently infectious cases, depending on the age / region)
+lambda[,] <- beta * cases_ij[i, j] / N
+
+## Default values of beta (average number of contacts)
+beta <- user(1.8)
+## Total population size
+N <- sum(S[,]) + sum(V1[,]) + sum(V2[,]) + sum(Es[,]) + sum(Ev1[,]) + sum(Ev2[,]) + 
+  sum(Is[,]) + sum(Iv1[,]) + sum(Iv2[,]) + sum(R[,])
+
+## Compute the potential of infection given the current number of infectious cases
+## in each age group / region. Transmission from cases A (age i, region j) to 
+## B (age k, region l) is m[i, k] * d[j, l]**-a
+## with m the age contact matrix, d the distance matrix, and a the spatial kernel parameter
+## Therefore, we compute cases_ijkl (the number of connection from [i,j] to [k,l])
+cases_ijkl[, , , ] <- m[i, k] * d_a[j, l] *
+  (Is[k, l] + vacc1 * Iv1[k, l] + vacc2 * Iv2[k, l])
+## with vacc1/ vacc2: protection from onwards transmission brought by vaccination 
+## and d_a the distance matrix after applying the spatial kernel
+d_a[,] <- (d[i, j]^(a))
+
+## We sum over k and l to get the total potential for transmission towards individuals 
+## in (i,j)
+cases_ijk[, , ] <- sum(cases_ijkl[i, j, k, ])
+cases_ij[, ] <- sum(cases_ijk[i, j, ])
+
+# Dimension of the transmission matrices
+dim(cases_ijkl) <- c(N_age, N_reg, N_age, N_reg)
+dim(cases_ijk) <- c(N_age, N_reg, N_age)
+dim(cases_ij) <- c(N_age, N_reg)
+# Dimension of the force of infection matrices
+dim(lambda) <- c(N_age, N_reg)
+dim(lambda_t) <- c(N_age, N_reg)
+# Dimension of the distance matrices and age contact matrix
+dim(d_a) <- c(N_reg, N_reg)
+dim(m) <- c(N_age, N_age)
+dim(d) <- c(N_reg, N_reg)
+
+# Default value of the matrices and parameters
+m[, ] <- user() # No default value, has to be defined by the user
+d[, ] <- user() # No default value, has to be defined by the user
+a <- user(2)
+N_age <- user(2)
+N_reg <- user(2)
+vacc1 <- user(.5)
+vacc2 <- user(.1)
+
+
+
+#### 5- Compute the number of importations
+### Extract the number of importation per age / region at time (step + 1)
+import_t[,] <- import[i, j, step + 1]
+
+## Define time step
+dt <- user(1)
+initial(time) <- 0
+update(time) <- (step + 1) * dt
+
+## Initialise the number of importations / dimensions of the matrices
+import[,,] <- user()
+dim(import) <- c(N_age, N_reg, N_time)
+dim(import_t) <- c(N_age, N_reg)
+N_time <- user(2)
+
+
+
+## 6- Initial conditions
+initial(S[,]) <- S_ini[i, j]
+initial(V1[,]) <- V1_ini[i, j]
+initial(V2[,]) <- V2_ini[i, j]
+initial(Es[,]) <- Es_ini[i, j]
+initial(Ev1[,]) <- Ev1_init[i, j]
+initial(Ev2[,]) <- Ev2_init[i, j]
+initial(Is[,]) <- Is_init[i, j]
+initial(Iv1[,]) <- Iv1_init[i, j]
+initial(Iv2[,]) <- Iv2_init[i, j]
+initial(R[,]) <- R_init[i, j]
+## Default values
+S_ini[,] <- user(100)
+V1_ini[,] <- user(100)
+V2_ini[,] <- user(800)
+Es_ini[,] <- user(0)
+Ev1_init[,] <- user(0)
+Ev2_init[,] <- user(0)
+Is_init[,] <- user(1)
+Iv1_init[,] <- user(0)
+Iv2_init[,] <- user(0)
+R_init[,] <- user(0)
+
+### Dimensions of each compartment
+dim(S) <- c(N_age, N_reg)
+dim(V1) <- c(N_age, N_reg)
+dim(V2) <- c(N_age, N_reg)
+dim(Es) <- c(N_age, N_reg)
+dim(Ev1) <- c(N_age, N_reg)
+dim(Ev2) <- c(N_age, N_reg)
+dim(Is) <- c(N_age, N_reg)
+dim(Iv1) <- c(N_age, N_reg)
+dim(Iv2) <- c(N_age, N_reg)
+dim(R) <- c(N_age, N_reg)
+
+### Initialise the dimensions of the compartments' initial value
+dim(S_ini) <- c(N_age, N_reg)
+dim(V1_ini) <- c(N_age, N_reg)
+dim(V2_ini) <- c(N_age, N_reg)
+dim(Es_ini) <- c(N_age, N_reg)
+dim(Ev1_init) <- c(N_age, N_reg)
+dim(Ev2_init) <- c(N_age, N_reg)
+dim(Is_init) <- c(N_age, N_reg)
+dim(Iv1_init) <- c(N_age, N_reg)
+dim(Iv2_init) <- c(N_age, N_reg)
+dim(R_init) <- c(N_age, N_reg)
+
+
+### Initialise the dimensions of the number of individual moving between the compartments
+dim(n_SEs) <- c(N_age, N_reg)
+dim(n_v1E) <- c(N_age, N_reg)
+dim(n_v2E) <- c(N_age, N_reg)
+dim(n_EsIs) <- c(N_age, N_reg)
+dim(n_Ev1Iv1) <- c(N_age, N_reg)
+dim(n_Ev12v2) <- c(N_age, N_reg)
+dim(n_IsR) <- c(N_age, N_reg)
+dim(n_Iv1R) <- c(N_age, N_reg)
+dim(n_Iv2R) <- c(N_age, N_reg)
+dim(p_SE) <- c(N_age, N_reg)
+dim(p_SEv1) <- c(N_age, N_reg)
+dim(p_SEv2) <- c(N_age, N_reg)
---
+++ R/script_generate_output.R
@@ -0,0 +1,233 @@
+## Script:
+## Import odin.dust model, define model variables, run model, plot outputs
+
+## 4 regions ; 3 age groups
+
+#### Import libraries ####
+
+library(dplyr)
+library(odin.dust)
+library(mcstate)
+library(tictoc)
+
+
+#### Define model and variables ####
+
+## Import odin.dust model
+si_age <- odin.dust::odin_dust("R/model_odin_dust.R")
+
+
+## Define number of contacts
+beta <- 1.5
+
+## Define vaccine efficacy
+# Against infection
+v1 <- .5
+v2 <- .01
+# Against onwards infection
+vacc1 <- .7
+vacc2 <- .5
+
+## Define spatial kernel parameter
+a <- 2.5
+
+## Define contact matrix
+ref_m <- matrix(, nrow = 3, ncol = 3)
+# The number of contacts between groups is smaller than within groups
+ref_m[] <- abs(rnorm(n = prod(dim(ref_m)), mean = .2, sd = .05))
+diag(ref_m)[] <- rnorm(n = nrow(ref_m), mean = .8, sd = .05)
+
+## Define distance matrix. Distance is defined as the degree of connectivity between
+## regions (i.e. distance of 1 between neighbours, 2 between neighbours of neighbours etc...)
+ref_d <- matrix(c(0, 1, 1, 2,
+                  1, 0, 2, 1,
+                  1, 2, 0, 2,
+                  2, 1, 2, 0), ncol = 4) + 1
+
+## Total number of inhabitants per region / age groups
+N <- matrix(c(2e3, 8e3, 1e4, 5e3, 
+              1.5e4, 3.5e4, 5e4, 
+              2e4, 5e4, 1e5, 1.5e5, 7e4), ncol = 4, byrow = T)
+
+## Number of inhabitants in each compartment
+# In the first age group, everyone is susceptible
+# In the third age group, most people are vaccinated twice
+# Low vaccine uptake in region 3 (1/3 of susceptibles)
+S <- matrix(c(2e3, 8e3, 1e4, 5e3, 
+              1.5e4 / 3, 3.5e4 / 4, 5e4 / 2, 2e4 / 8, 
+              5e4 / 10, 1e5 / 20, 1.5e5 / 3, 7e4 / 100), ncol = 4, byrow = T)
+V1 <- matrix(c(0, 0, 0, 0,
+               1.5e4 / 6, 3.5e4 / 8, 5e4 / 8, 2e4 / 4, 
+               5e4 / 20, 1e5 / 20, 1.5e5 / 20, 7e4 / 20), ncol = 4, byrow = T)
+V2 <- matrix(c(0, 0, 0, 0, 
+               1.5e4 / 8, 3.5e4 / 10, 5e4 / 10, 2e4 / 10, 
+                3 * 5e4 / 4, 8 * 1e5 / 10, 1.5e5 / 2, 9 * 7e4 / 10), ncol = 4, byrow = T)
+
+R <- N - S - V1 - V2
+if(any(R < 0)) stop()
+
+## Define the seasonality parameters
+X <- .2
+Y <- .4
+
+## Define time step
+dt <- 1
+## Define the number of particules (i.e. the number of stochastic runs)
+n_part <- 500
+
+## Duration of the run (in days)
+t_tot <- 365 * 10
+
+## Mean number of import by day
+mean_import <- exp(.2 * cos(2 * 3.14 * seq_len(t_tot) / 365) + 
+                     1.5 * sin(2 * 3.14 * seq_len(t_tot) / 365) - 4)
+# Draw the number of importations (by age, region, and day)
+import <- array(round(rpois(n = t_tot * nrow(ref_m) * nrow(ref_d), 
+                            rep(mean_import, each = nrow(ref_m) * nrow(ref_d)))),
+                dim = c(nrow(ref_m), nrow(ref_d), t_tot))
+
+#### Model runs ####
+
+## Initialise the model using the parameters previously defined
+seir_model <- si_age$new(pars = list(m = ref_m, d = 1/ref_d, a = a, 
+                                     beta = beta, X = X, Y = Y,
+                                     v1 = v1, v2 = v2, vacc1 = vacc1, vacc2 = vacc2, 
+                                     import = import, 
+                                     N_time = dim(import)[3], N_age = nrow(ref_m), N_reg = nrow(ref_d), 
+                                     S_ini = S, V1_ini = V1, V2_ini = V2, 
+                                     Es_ini = S * 0, Ev1_init = S * 0, Ev2_init = S * 0,
+                                     Is_init = S * 0, Iv1_init = S * 0, Iv2_init = S * 0,
+                                     R_init = R, 
+                                     dt = 1),
+                        step = 1, n_particles = n_part, n_threads = 1L, seed = 1L)
+
+
+## Define array output_sim, containing the number of individuals in each compartment per day
+## in each simulation
+output_sim <- array(NA, dim = c(seir_model$info()$len, n_part, t_tot))
+
+## Rename the rows of output_sim, as the name of the state + region + age group
+states <- c("S", "V1", "V2", "Es", "Ev1", "Ev2", "Is", "Iv1", "Iv2", "R")
+## The first row of output_sim contains the time
+rownames(output_sim) <- c("Time",
+  paste0(rep(paste0(rep(states, each = nrow(ref_d)), 
+                    "_reg", rep(seq_len(nrow(ref_d)), 10)), each = nrow(ref_m)), 
+         "_age", seq_len(nrow(ref_m)))
+)
+
+# For loop to run the model iteratively and store the results in output_sim
+for (t in seq_len(t_tot)) {
+  output_sim[ , , t] <- seir_model$run(t)
+}
+
+#### Analyse simulations / generate plots ####
+
+## Extract time and number of individuals per compartmemt
+time <- output_sim[1, 1, ]
+output <- output_sim[-1, , ]
+
+## Extract the distribution of the population at the last time step
+final_res <- output_sim[, , dim(output)[3]]
+
+## Compute the number of cases per region / age group across simulations
+print(apply(final_res[grep("R_", rownames(final_res)), ] - c(R), 1, summary))
+## Compute the total number of cases per region across simulations
+print(summary(colSums(final_res[grep("R_", rownames(final_res)), ] - c(R))))
+## Compute the total number of single vaccinated cases per region across simulations
+print(summary(colSums(c(V1) - final_res[grep("V1_", rownames(final_res)), ])))
+## Compute the total number of double vaccinated cases per region across simulations
+print(summary(colSums(c(V2) - final_res[grep("V2_", rownames(final_res)), ])))
+
+
+
+### Generate plot of the number of Susceptibles, Infected and Recovered individuals through time
+### per region / age group
+png(filename = "Output/fig_sir_import.png", width = 600, height = 800)
+par(mfrow = c(nrow(ref_d),nrow(ref_m)), mar = c(3, 4, 2, 0.5), mar = c(3, 4, 1, 0), 
+    oma = c(2, 2, 0, 2), las = 1, bty = "l")
+
+## Define colour scheme
+cols <- c(S = "#8c8cd9", I = "#cc0044", R = "#999966")
+
+for(i in seq_len(nrow(ref_d))){
+  for(j in seq_len(nrow(ref_m))){
+    ## Extract entries from region i and age j
+    lab_i_j <- paste0("_reg", i, "_age", j)
+    ## Compute total number of susceptibles + vaccinated
+    susceptibles <- colSums(output[c(paste0("S", lab_i_j), paste0("V1", lab_i_j), paste0("V2", lab_i_j)),, ])
+    ## Compute total number of infectious
+    infected <- colSums(output[c(paste0("Is", lab_i_j), paste0("Iv1", lab_i_j), paste0("Iv2", lab_i_j)),, ])
+    ## Compute total number of recovered
+    recovered <- output[c(paste0("R", lab_i_j)),, ]
+    
+    ## Plot the number of susceptible individuals through time
+    matplot(time, t(susceptibles), type = "l", xlab = "", ylab = "", yaxt="none", 
+            main = paste0("Age ", j, "Region", i), col = cols[["S"]], lty = 1, 
+            ylim = range(c(susceptibles, infected, recovered)))
+    
+    ## Add the number of infected
+    matlines(time, t(infected), col = cols[["I"]], lty = 1)
+    
+    ## Add the number of recovered
+    matlines(time, t(recovered), col = cols[["R"]], lty = 1)
+    
+    ## Add the legend in the topleft panel
+    if(i == 1 & j == 1)
+      legend("right", lwd = 1, col = cols, legend = names(cols), bty = "n")
+    axis(2, las =2)
+  }
+}
+
+# Add axis names
+title(xlab = "Time (days)", outer = T, line = 0, cex.lab = 2)
+title(ylab = "Number of individuals", outer = T, line = 0, cex.lab = 2)
+
+dev.off()
+
+### Generate plot of the number of Infected per region / age group, stratified by vaccination status
+png(filename = "Output/fig_new_cases_import.png", width = 600, height = 800)
+par(mfrow = c(nrow(ref_d),nrow(ref_m)), mar = c(3, 4, 2, 0.5), mar = c(3, 4, 1, 0), 
+    oma = c(2, 2, 0, 2), las = 1, bty = "l")
+
+## Define colour scheme
+cols <- c(Is = "#8c8cd9", Iv1 = "#cc0044", Iv2 = "#999966")
+
+for(i in seq_len(nrow(ref_d))){
+  for(j in seq_len(nrow(ref_m))){
+    # Extract the rownames of all rows of interest
+    lab_i_j <- c(paste0("Is_reg", i, "_age", j), paste0("Iv1_reg", i, "_age", j),
+                 paste0("Iv2_reg", i, "_age", j))
+    
+    ## Plot the number of non-vaccinated infectious individuals at each date
+    matplot(time, t(output[lab_i_j[1],, ]), type = "l", 
+            xlab = "", ylab = "", yaxt="none", main = paste0("Age ", j, "Region", i),
+            col = cols[["Is"]], lty = 1, ylim=range(output[lab_i_j,,]))
+    
+    ## Add the number of single-vaccinated infectious indviduals
+    matlines(time, t(output[lab_i_j[2], , ]), col = cols[["Iv1"]], lty = 1)
+    
+    ## Add the number of double-vaccinated infectious indviduals
+    matlines(time, t(output[lab_i_j[3], , ]), col = cols[["Iv2"]], lty = 1)
+    
+    ## Add a legend to the topleft panel
+    if(i == 1 & j == 1)
+      legend("right", lwd = 1, col = cols, legend = names(cols), bty = "n")
+    
+    axis(2, las =2)
+    
+  }
+}
+# Add axis names
+title(xlab = "Time (days)", outer = T, line = 0, cex.lab = 2)
+title(ylab = "Number of individuals", outer = T, line = 0, cex.lab = 2)
+
+dev.off()
+
+## Plot beta_t, the number of contacts (accounting for seasonality) 
+par(mfrow = c(2, 1), mar = c(3, 4, 2, 0.5), mar = c(3, 4, 1, 0), 
+    oma = c(2, 2, 0, 2), las = 1, bty = "l")
+plot(beta * exp(X * cos(2 * 3.14 * (1:365) / 365) + Y * sin(2 * 3.14 * (1:365) / 365)),
+     ylab = "Nb of infectious contacts", xlab = "Time (days)")
+## Plot the number of importations per day across the simulation period
+plot(apply(import, 3, sum), type = "l", 
+     ylab = "Nb of importations", xlab = "Time (days)")
