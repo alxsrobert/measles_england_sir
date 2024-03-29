b0fca597c78b38d95fa46c03dd7021543e609fe7
alxsrobert <alexis3.robert3@gmail.com>
2024-03-29 16:35:27 +0000

    Script to generate stochastic simulations from model fits

---
+++ R/generate_outbreak_all_samples.R
@@ -0,0 +1,50 @@
+source("R/import_library.R")
+
+n_part <- 10
+n_samples <- 500
+
+## Generate model specifications
+list_specs_run <- specs_run()
+
+## Create the four different scenario
+## - main scenario: cprd vaccine data, distance kernel parameter estimated, no baseline leak
+## - sensitivity: baseline leak (constant with age)
+## - sensitivity: cover vaccine data
+## - sensitivity: fixed distance kernel
+input_parameters <- 
+  list(list(vax = "cprd", distance = "degree", sec = FALSE), 
+       list(vax = "cprd", distance = "degree", sec = TRUE), 
+       list(vax = "cover", distance = "degree", sec = FALSE),
+       list(vax = "cprd", distance = "fixed", sec = FALSE))
+
+## Three waning scenarios
+vec_waning <- c("no", "since_eli", "since_vax")
+
+
+for(i in seq_along(input_parameters)){
+  # Extract vax, distance and sec from the input_parameters list
+  vax <- input_parameters[[i]]$vax
+  distance <- input_parameters[[i]]$distance
+  sec <- input_parameters[[i]]$sec
+  
+  for(waning in vec_waning){
+    ## Read object containing model and parameter fits
+    pmcmc_run <- readRDS(paste0("Output/", vax, "_", distance, if(sec) "_sec", "/",
+                                waning, ".RDS"))
+    ## Generate stochastic simulations (n_part * n_samples simulations in total)
+    all_output <- generate_outbreaks(pmcmc_run, list_specs_run, vax, n_part, n_samples, waning,
+                                     burnin = 1000)
+    ## save 3d array containing the stochastic simulations per status and year
+    saveRDS(all_output, paste0("Output/", vax, "_", distance, if(sec) "_sec", "/sim_",
+                               waning, ".RDS"))
+    if(waning == "since_eli"){
+      ## Generate stochastic simulations (n_part * n_samples simulations in total) with waning removed
+      all_output <- generate_outbreaks(pmcmc_run, list_specs_run, vax, n_part, n_samples, 
+                                       waning, burnin = 1000, nowane = TRUE)
+      ## save 3d array containing the stochastic simulations per status and year
+      saveRDS(all_output, paste0("Output/", vax, "_", distance, if(sec) "_sec", "/sim_",
+                                 waning, "_nowane.RDS"))
+    }
+  }
+}
+
